183b68b6b6d72aa18d536b82d6f0ca197f981619
Saagar Jha <saagar@saagarjha.com>
2022-05-23 19:54:40 -0700

    Add files

---
+++ EffectivePower/ChartView.swift
@@ -0,0 +1,153 @@
+//
+//  ChartView.swift
+//  EffectivePower
+//
+//  Created by Saagar Jha on 5/23/22.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+	let document: EffectivePowerDocument
+
+	@Binding
+	var magnification: CGFloat
+	@GestureState
+	var partialMagnification: CGFloat = 1
+	@Binding
+	var selectedRange: ClosedRange<TimeInterval>?
+	@State
+	var location: CGPoint?
+
+	static let overlayFormatter: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .short
+		formatter.timeStyle = .short
+		return formatter
+	}()
+
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack {
+				let bounds = document.bounds.lowerBound.timeIntervalSince1970...document.bounds.upperBound.timeIntervalSince1970
+				let width = geometry.size.width * magnification * partialMagnification
+				ScrollView(.horizontal) {
+					ZStack {
+						// Hide when in a gesture, because drawing this is slow
+						if partialMagnification == 1 {
+							HistogramGraphView(horizontalBounds: bounds, values: document.events, color: Color(hue: 0.53, saturation: 1, brightness: 1), identityToken: document)
+								.equatable()
+								.frame(width: width)
+						}
+						BinaryGraphView(bounds: bounds, values: document.batteryStatuses, color: Color.green.opacity(0.5), identityToken: document)
+							.equatable()
+							.frame(width: width)
+						LinearGraphView(horizontalBounds: bounds, verticalBounds: 0...100, values: document.batteryStatuses, color: Color.foregroundish, identityToken: document)
+							.equatable()
+							.frame(width: width)
+						if let selectedRange = selectedRange {
+							let start = max((selectedRange.lowerBound - bounds.lowerBound) / (bounds.upperBound - bounds.lowerBound) * width, 0)
+							let end = min((selectedRange.upperBound - bounds.lowerBound) / (bounds.upperBound - bounds.lowerBound) * width, width)
+							let veilColor = Color.backgroundish.opacity(0.5)
+							let boundColor = Color.foregroundish
+							Rectangle()
+								.fill(veilColor)
+								.frame(width: start, height: geometry.size.height)
+								.position(x: start / 2, y: geometry.size.height / 2)
+							Path { path in
+								path.move(to: .init(x: start, y: 0))
+								path.addLine(to: .init(x: start, y: geometry.size.height))
+							}.stroke(boundColor)
+							Rectangle()
+								.fill(veilColor)
+								.frame(width: width - end, height: geometry.size.height)
+								.position(x: end + (width - end) / 2, y: geometry.size.height / 2)
+							Path { path in
+								path.move(to: .init(x: end, y: 0))
+								path.addLine(to: .init(x: end, y: geometry.size.height))
+							}
+							.stroke(boundColor)
+
+						}
+
+						MouseTrackingView(location: $location)
+
+						if let location = location {
+							Path { path in
+								path.move(to: .init(x: location.x, y: 0))
+								path.addLine(to: .init(x: location.x, y: geometry.size.height))
+							}
+							.stroke(style: StrokeStyle(dash: [5]))
+							.stroke(Color.foregroundish)
+						}
+					}
+					.gesture(
+						MagnificationGesture()
+							.updating($partialMagnification) { new, old, _ in
+								old = normalizePartialMagnification(new)
+							}
+							.onEnded {
+								magnification *= normalizePartialMagnification($0)
+								withAnimation {
+									magnification = max(1, magnification)
+								}
+							}
+					)
+					.gesture(
+						DragGesture()
+							.onChanged {
+								let drag = CGRect(origin: $0.startLocation, size: $0.translation).standardized
+								selectedRange = (bounds.lowerBound + drag.minX / width * (bounds.upperBound - bounds.lowerBound))...((bounds.lowerBound + drag.maxX / width * (bounds.upperBound - bounds.lowerBound)))
+								location = $0.location
+							}
+					)
+					.gesture(
+						TapGesture()
+							.onEnded {
+								selectedRange = nil
+							}
+					)
+				}.frame(width: geometry.size.width)
+				if let location = location {
+					// Hack to align the view to the bottom right corner
+					HStack {
+						VStack {
+							Spacer()
+							let time = bounds.lowerBound + (location.x / width) * (bounds.upperBound - bounds.lowerBound)
+							let closestBatteryStatus = document.batteryStatuses.min {
+								abs($0.timestamp.timeIntervalSince1970 - time) < abs($1.timestamp.timeIntervalSince1970 - time)
+							}!
+							HStack {
+								HStack {
+									Image(systemName: "clock.fill")
+									Text("\(Date(timeIntervalSince1970: time), formatter: Self.overlayFormatter)")
+										.font(.title3.monospacedDigit())
+								}
+								Divider()
+								HStack {
+									Image(systemName: closestBatteryStatus.charging ? "battery.100.bolt" : "battery.100")
+									Text(String(format: "%.0f%%", closestBatteryStatus.level))
+										.font(.title3.monospacedDigit())
+								}
+							}
+							.padding()
+							.fixedSize(horizontal: false, vertical: true)
+							.materialBackground()
+							.cornerRadius(8)
+						}
+						Spacer()
+					}
+					.padding(20)
+				}
+			}
+		}
+	}
+
+	func normalizePartialMagnification(_ partialMagnification: CGFloat) -> CGFloat {
+		if magnification * partialMagnification < 1 {
+			return 1 - pow(1 - partialMagnification, 2)
+		} else {
+			return partialMagnification
+		}
+	}
+}
---
+++ EffectivePower/EffectivePowerDocument.swift
@@ -0,0 +1,190 @@
+//
+//  EffectivePowerDocument.swift
+//  EffectivePower
+//
+//  Created by Saagar Jha on 5/8/22.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+	static var plsql = Self(importedAs: "com.saagarjha.plsql")
+}
+
+struct Node: Hashable, Identifiable {
+	let name: String
+
+	var id: String {
+		name
+	}
+}
+
+class Event: Identifiable {
+	let node: Node?
+	let rootNode: Node?
+	weak var parent: Event?
+	let timestamp: ClosedRange<Date>
+	var energy: Int
+
+	var children = [Event]()
+
+	init(node: Node?, rootNode: Node?, parent: Event?, timestamp: ClosedRange<Date>, energy: Int) {
+		self.node = node
+		self.rootNode = rootNode
+		self.parent = parent
+		self.timestamp = timestamp
+		self.energy = energy
+	}
+}
+
+class BatteryStatus: Hashable, Identifiable {
+	let timestamp: Date
+	let level: Double
+	let charging: Bool
+
+	init(timestamp: Date, level: Double, charging: Bool) {
+		self.timestamp = timestamp
+		self.level = level
+		self.charging = charging
+	}
+
+	static func == (lhs: BatteryStatus, rhs: BatteryStatus) -> Bool {
+		ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+	}
+
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(ObjectIdentifier(self))
+	}
+}
+
+class EffectivePowerDocument: FileDocument, Equatable {
+	static var readableContentTypes: [UTType] { [.plsql] }
+
+	let nodes: [Int: Node]
+	let events: [Event]
+	let batteryStatuses: [BatteryStatus]
+
+	let bounds: ClosedRange<Date>
+
+	let deviceName: String?
+	let deviceModel: String?
+	let deviceBuild: String?
+
+	init() {
+		nodes = [:]
+		events = []
+		batteryStatuses = []
+		deviceName = ""
+		deviceModel = ""
+		deviceBuild = ""
+		bounds = Date()...(Date())
+	}
+
+	required init(configuration: ReadConfiguration) throws {
+		let database = try SQLiteDatabase(data: configuration.file.regularFileContents!)
+		var nodes = [Int: Node]()
+		for row in try database.execute(statement: "SELECT ID, Name FROM 'PLAccountingOperator_EventNone_Nodes'") {
+			let row = row.map {
+				$0!
+			}
+			nodes[Int(row[0])!] = Node(name: row[1])
+		}
+		self.nodes = nodes
+		
+		var offsets = [(Double, Double)]()
+		for row in try database.execute(statement: "SELECT timestamp, system FROM PLStorageOperator_EventForward_TimeOffset") {
+			let row = row.map {
+				$0!
+			}
+			let timestamp = Double(row[0])!
+			let offset = Double(row[1])!
+			offsets.append((timestamp, offset))
+		}
+		offsets.sort {
+			$0.0 < $1.0
+		}
+		
+		func offset(for timestamp: Double) -> Double {
+			offsets[offsets.index(offsets.firstIndex {
+				$0.0 > timestamp
+			} ?? offsets.endIndex, offsetBy: -1, limitedBy: offsets.startIndex) ?? offsets.startIndex].1
+		}
+
+		var events = [Int: Event]()
+		for row in try database.execute(statement: "SELECT ID, NodeID, RootNodeID, ParentEntryID, timestamp, StartOffset, EndOffset, Energy, CorrectionEnergy FROM 'PLAccountingOperator_EventInterval_EnergyEstimateEvents'") {
+			let row = row.map {
+				$0!
+			}
+			let timestamp = Double(row[4])!
+			let offset = offset(for: timestamp)
+			let startOffset = Double(row[5])!
+			let endOffset = Double(row[6])!
+			let _start = Date(timeIntervalSince1970: timestamp + startOffset / 1_000)
+			let _end = Date(timeIntervalSince1970: timestamp + endOffset / 1_000)
+			// Ignore these, as per Apple's response to FB11722856:
+			// These are the “dummy’ power events…created with a startDate of NSDate distantPast
+			guard abs(_start.timeIntervalSince(.distantPast)) >= 1,
+				  abs(_end.timeIntervalSince(.distantPast)) >= 1 else {
+				print("Ignoring dummy events: ", _start, _end)
+				continue
+			}
+			let start = _start.addingTimeInterval(offset)
+			let end = _end.addingTimeInterval(offset)
+			if start == end {
+				continue
+			}
+			let parent = events[Int(row[3])!]
+			let event = Event(node: nodes[Int(row[1])!], rootNode: nodes[Int(row[2])!], parent: parent, timestamp: start...end, energy: Int(row[7])! + Int(row[8])!)
+			parent?.children.append(event)
+			events[Int(row[0])!] = event
+		}
+		self.events = Array(events.values)
+
+		var roots = self.events.filter {
+			$0.parent == nil
+		}
+
+		while let event = roots.popLast() {
+			event.energy -= event.children.map(\.energy).reduce(0, +)
+			roots.append(contentsOf: event.children)
+		}
+
+		var batteryStatus = [BatteryStatus]()
+		for row in try database.execute(statement: "SELECT timestamp, Level, ExternalConnected FROM 'PLBatteryAgent_EventBackward_Battery'") {
+			let row = row.map {
+				$0!
+			}
+			let timestamp = Double(row[0])!
+			let offset = offset(for: timestamp)
+			batteryStatus.append(BatteryStatus(timestamp: Date(timeIntervalSince1970: timestamp + offset), level: Double(row[1])!, charging: Int(row[2])! != 0))
+		}
+		self.batteryStatuses = batteryStatus.sorted {
+			$0.timestamp < $1.timestamp
+		}
+
+		let configuration = try database.execute(statement: "SELECT timestamp, DeviceName, Device, Build FROM 'PLConfigAgent_EventNone_Config' ORDER BY timestamp")
+		deviceName =
+			configuration.compactMap {
+				$0[1]
+			}.last
+		deviceModel =
+			configuration.compactMap {
+				$0[2]
+			}.last
+		deviceBuild =
+			configuration.compactMap {
+				$0[3]
+			}.last
+
+		bounds = min(batteryStatuses.first!.timestamp, self.events.map(\.timestamp.lowerBound).min()!)...max(batteryStatuses.last!.timestamp, self.events.map(\.timestamp.upperBound).max()!)
+	}
+
+	func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+		configuration.existingFile!
+	}
+
+	static func == (lhs: EffectivePowerDocument, rhs: EffectivePowerDocument) -> Bool {
+		return lhs === rhs
+	}
+}
